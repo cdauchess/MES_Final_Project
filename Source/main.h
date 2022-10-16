fd9a76a2ff34e74f8bfe295cf21a02fd1cda14a4
cdauchess <charlie.dauchess@gmail.com>
2022-10-15 23:00:41 -0400

    Neopixel Code
    Neopixel file added.  Largely taken from the RP2040 examples with some adjustments for use in my project.  I also reorganized the code into a source folder for better overall project organization in the future.

---
+++ Source/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "generated/ws2812.pio.h"
+
+#include "neopixel.h"
